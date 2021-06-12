78b89df99c58a6d052336d7548d43108542f007e
Topsy Chen <62056970+thomaswcy@users.noreply.github.com>
2021-06-12 17:17:51 +0800

    Update lean_wan-lan.sh

--- scripts/lean_wan-lan.sh
+++ scripts/lean_wan-lan.sh
@@ -19,7 +19,6 @@ git clone --depth=1 https://github.com/Beginner-Go/luci-app-koolproxyR
 git clone --depth=1 https://github.com/garypang13/luci-app-bypass
 svn co https://github.com/garypang13/openwrt-packages/trunk/lua-maxminddb
 git clone --depth=1 https://github.com/garypang13/smartdns-le
-#sed -i 's/smartdns-le/smartdns/g' luci-app-bypass/Makefile
 
 # Add mentohust & luci-app-mentohust
 git clone --depth=1 https://github.com/BoringCat/luci-app-mentohust
@@ -148,7 +147,7 @@ sed -i "s,'eth1' 'eth0','eth0' 'eth1',g" target/linux/rockchip/armv8/base-files/
 sed -i 's/\/bin\/ash/\/usr\/bin\/zsh/g' package/base-files/files/etc/passwd
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.8.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 sed -i '/uci commit system/i\uci set system.@system[0].hostname='RubikWrt'' package/lean/default-settings/files/zzz-default-settings
 sed -i "s/RubikWrt /thomaswcy build $(TZ=UTC-8 date "+%Y.%m.%d") @ RubikWrt /g" package/lean/default-settings/files/zzz-default-settings
 # sed -i "s/FILES:=$(LINUX_DIR)/net/can/can-dev.ko/FILES:=$(LINUX_DIR)/drivers/net/can/dev/can-dev.ko \ package/kernel/linux/modules/can.mk
