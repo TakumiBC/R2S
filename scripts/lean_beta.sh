1e9a1b7e431974a4d7cea32d2872bec9e404444a
Topsy Chen <62056970+thomaswcy@users.noreply.github.com>
2021-06-12 17:19:09 +0800

    Update lean_beta.sh

--- scripts/lean_beta.sh
+++ scripts/lean_beta.sh
@@ -159,7 +159,7 @@ popd
 sed -i 's/\/bin\/ash/\/usr\/bin\/zsh/g' package/base-files/files/etc/passwd
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.8.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 sed -i '/uci commit system/i\uci set system.@system[0].hostname='FusionWrt'' package/lean/default-settings/files/zzz-default-settings
 sed -i "s/RubikWrt /thomaswcy build $(TZ=UTC-8 date "+%Y.%m.%d") @ RubikWrt /g" package/lean/default-settings/files/zzz-default-settings
 
